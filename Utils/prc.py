f7994b506daf85fa7875bc021467e463c04b4e20
Shashank Pandey <jpshashank200@gmail.com>
2025-02-09 17:35:07 +0530

    umm idk

--- Utils/prc.py
+++ Utils/prc.py
@@ -114,7 +114,7 @@ class PRC_API_Client:
         await self.session.close()
 
     async def fetch_server_key(self, server_id: int):
-        server_key = await self.bot.settings.find_one(
+        server_key = await self.bot.settings.db.find_one(
             {
                 "guild_id": server_id,
             }
